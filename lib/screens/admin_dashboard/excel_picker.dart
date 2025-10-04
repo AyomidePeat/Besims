f0dc89744de4c95eb949643f4551b92cf32b87be
ayomidepeat <peatayomide@gmail.com>
2025-10-04 11:39:35 +0100

    fix: correct excel downloader and excel picker to match most recent package upgrade

--- lib/screens/admin_dashboard/excel_picker.dart
+++ lib/screens/admin_dashboard/excel_picker.dart
@@ -41,42 +41,42 @@ Future<void> importInventoryFile(pickExcelFile) async {
             ? (row[0]!.value as SharedString).toString()
             : (row[0]!.value is int)
                 ? (row[0]!.value as int).toString()
-                : row[0]!.value ?? '';
+                : row[0]!.value?.toString() ?? '';
         final category = (row[1]!.value is SharedString)
             ? (row[1]!.value as SharedString).toString()
             : (row[1]!.value is int)
                 ? (row[1]!.value as int).toString()
-                : row[1]!.value ?? '';
+                : row[1]!.value?.toString() ?? '';
         final expiryDate = (row[2]!.value is SharedString)
             ? (row[2]!.value as SharedString).toString()
             : (row[2]!.value is int)
                 ? (row[2]!.value as int).toString()
-                : row[2]!.value ?? '';
+                : row[2]!.value?.toString() ?? '';
         final costPrice = (row[3]!.value is SharedString)
             ? (row[3]!.value as SharedString).toString()
             : (row[3]!.value is int)
                 ? (row[3]!.value as int).toString()
-                : row[3]!.value ?? '';
+                : row[3]!.value?.toString() ?? '';
         final sellingPrice = (row[4]!.value is SharedString)
             ? (row[4]!.value as SharedString).toString()
             : (row[4]!.value is int)
                 ? (row[4]!.value as int).toString()
-                : row[4]!.value ?? '';
+                : row[4]!.value?.toString() ?? '';
         final supplier = (row[5]!.value is SharedString)
             ? (row[5]!.value as SharedString).toString()
             : (row[5]!.value is int)
                 ? (row[5]!.value as int).toString()
-                : row[5]!.value ?? '';
+                : row[5]!.value?.toString() ?? '';
         final quantity = (row[6]!.value is SharedString)
             ? (row[6]!.value as SharedString).toString()
             : (row[6]!.value is int)
                 ? (row[6]!.value as int).toString()
-                : row[6]!.value ?? '';
+                : row[6]!.value?.toString() ?? '';
         final status = (row[7]!.value is SharedString)
             ? (row[7]!.value as SharedString).toString()
             : (row[7]!.value is int)
                 ? (row[7]!.value as int).toString()
-                : row[7]!.value ?? '';
+                : row[7]!.value?.toString() ?? '';
 
         firestoreClass.addStockInventory(
           name: productName,
@@ -107,27 +107,27 @@ Future<void> importSupplierFile(pickExcelFile) async {
             ? (row[0]!.value as SharedString).toString()
             : (row[0]!.value is int)
                 ? (row[0]!.value as int).toString()
-                : row[0]!.value ?? '';
+                : row[0]!.value?.toString() ?? '';
         final company = (row[1]!.value is SharedString)
             ? (row[1]!.value as SharedString).toString()
             : (row[1]!.value is int)
                 ? (row[1]!.value as int).toString()
-                : row[1]!.value ?? '';
+                : row[1]!.value?.toString() ?? '';
         final address = (row[2]!.value is SharedString)
             ? (row[2]!.value as SharedString).toString()
             : (row[2]!.value is int)
                 ? (row[2]!.value as int).toString()
-                : row[2]!.value ?? '';
+                : row[2]!.value?.toString() ?? '';
         final email = (row[3]!.value is SharedString)
             ? (row[3]!.value as SharedString).toString()
             : (row[3]!.value is int)
                 ? (row[3]!.value as int).toString()
-                : row[3]!.value ?? '';
+                : row[3]!.value?.toString() ?? '';
         final phone = (row[4]!.value is SharedString)
             ? (row[4]!.value as SharedString).toString()
             : (row[4]!.value is int)
                 ? (row[4]!.value as int).toString()
-                : row[4]!.value ?? '';
+                : row[4]!.value?.toString() ?? '';
         final now = DateTime.now();
         String currentDate = DateFormat('dd/mm/yyyy').format(now);
 
@@ -158,27 +158,27 @@ Future<void> importStoreFile(pickExcelFile) async {
             ? (row[0]!.value as SharedString).toString()
             : (row[0]!.value is int)
                 ? (row[0]!.value as int).toString()
-                : row[0]!.value ?? '';
+                : row[0]!.value?.toString() ?? '';
         final location = (row[1]!.value is SharedString)
             ? (row[1]!.value as SharedString).toString()
             : (row[1]!.value is int)
                 ? (row[1]!.value as int).toString()
-                : row[1]!.value ?? '';
+                : row[1]!.value?.toString() ?? '';
         final manager = (row[2]!.value is SharedString)
             ? (row[2]!.value as SharedString).toString()
             : (row[2]!.value is int)
                 ? (row[2]!.value as int).toString()
-                : row[2]!.value ?? '';
+                : row[2]!.value?.toString() ?? '';
         final phone = (row[3]!.value is SharedString)
             ? (row[3]!.value as SharedString).toString()
             : (row[3]!.value is int)
                 ? (row[3]!.value as int).toString()
-                : row[3]!.value ?? '';
+                : row[3]!.value?.toString() ?? '';
         final status = (row[4]!.value is SharedString)
             ? (row[4]!.value as SharedString).toString()
             : (row[4]!.value is int)
                 ? (row[4]!.value as int).toString()
-                : row[4]!.value ?? '';
+                : row[4]!.value?.toString() ?? '';
 
         firestoreClass.addStore(
             name: name,
@@ -205,48 +205,48 @@ Future<void> importOrderFile(pickExcelFile) async {
             ? (row[0]!.value as SharedString).toString()
             : (row[0]!.value is int)
                 ? (row[0]!.value as int).toString()
-                : row[0]!.value ?? '';
+                : row[0]!.value?.toString() ?? '';
         final category = (row[1]!.value is SharedString)
             ? (row[1]!.value as SharedString).toString()
             : (row[1]!.value is int)
                 ? (row[1]!.value as int).toString()
-                : row[1]!.value ?? '';
+                : row[1]!.value?.toString() ?? '';
         final unitPrice = (row[2]!.value is SharedString)
             ? (row[2]!.value as SharedString).toString()
             : (row[2]!.value is int)
                 ? (row[2]!.value as int).toString()
-                : row[2]!.value ?? '';
+                : row[2]!.value?.toString() ?? '';
         final stockQty = (row[3]!.value is SharedString)
             ? (row[3]!.value as SharedString).toString()
             : (row[3]!.value is int)
                 ? (row[3]!.value as int).toString()
-                : row[3]!.value ?? '';
+                : row[3]!.value?.toString() ?? '';
         final quantity = (row[4]!.value is SharedString)
             ? (row[4]!.value as SharedString).toString()
             : (row[4]!.value is int)
                 ? (row[4]!.value as int).toString()
-                : row[4]!.value ?? '';
+                : row[4]!.value?.toString() ?? '';
         final seller = (row[5]!.value is SharedString)
             ? (row[5]!.value as SharedString).toString()
             : (row[5]!.value is int)
                 ? (row[5]!.value as int).toString()
-                : row[5]!.value ?? '';
+                : row[5]!.value?.toString() ?? '';
         final paymentMethod = (row[6]!.value is SharedString)
             ? (row[6]!.value as SharedString).toString()
             : (row[6]!.value is int)
                 ? (row[6]!.value as int).toString()
-                : row[6]!.value ?? '';
+                : row[6]!.value?.toString() ?? '';
 
         final status = (row[7]!.value is SharedString)
             ? (row[7]!.value as SharedString).toString()
             : (row[7]!.value is int)
                 ? (row[7]!.value as int).toString()
-                : row[7]!.value ?? '';
+                : row[7]!.value?.toString() ?? '';
         final costPrice = (row[8]!.value is SharedString)
             ? (row[8]!.value as SharedString).toString()
             : (row[8]!.value is int)
                 ? (row[8]!.value as int).toString()
-                : row[8]!.value ?? '';
+                : row[8]!.value?.toString() ?? '';
 
         firestoreClass.addProduct(
             category: category,
